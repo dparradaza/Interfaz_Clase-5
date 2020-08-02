c1d099af41728ea33baf1812438de1fb6d4500f6
dparradaza <daparrad@gmail.com>
2020-08-01 21:07:49 -0500

    inicializar

---
+++ Clase5/src/App/cliente/VentanaPrincipal/VentanaPrincipalTemplate.java
@@ -0,0 +1,23 @@
+package App.cliente.VentanaPrincipal;
+/***
+ * 
+ * @author diego
+ *
+ */
+import javax.swing.JFrame;
+
+public class VentanaPrincipalTemplate extends JFrame {
+
+	public VentanaPrincipalTemplate() {
+
+		super("Vista Principal");
+
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(1200, 700);
+		setLocationRelativeTo(this);
+		setLayout(null);
+		setVisible(true);
+
+	}
+
+}
---
+++ Clase5/src/App/main.java
@@ -0,0 +1,13 @@
+package App;
+
+import App.cliente.login.loginTemplate;
+
+public class main {
+
+	public static void main(String[] args) {
+		loginTemplate v1= new loginTemplate();
+		
+
+	}
+
+}
